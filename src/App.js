05f845aef18c2df7f78f5bdf0db0d0a5a9aac579
Alex <example@gmail.com>
2022-10-26 18:12:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
-import { Users } from './components/Users';
+import { Users } from './components/Users/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+  const [invites, setInvites] = useState([]);
+  const [sucess, setSucess] = useState(false)
+
+  const onClickInvited = (id) => {
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id))
+    } else
+      setInvites(prev => [...prev, id])
+  }
+
+
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(res => { setUsers(res.data) })
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false));
+  }, [])
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {sucess ? (<Success count={invites.length} />) :
+        (<Users items={users}
+          isLoading={isLoading}
+          setSearchValue={setSearchValue}
+          searchValue={searchValue}
+          onClickInvited={onClickInvited}
+          invites={invites}
+          setSucess={setSucess} />
+        )}
     </div>
   );
 }
